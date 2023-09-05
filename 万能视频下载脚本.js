cc96ff067fe17123565d56407290895b535f9a0a
pheonix <115352360+pheonixchen@users.noreply.github.com>
2023-09-05 20:59:52 +0800

    Update 万能视频下载脚本.js

--- 万能视频下载脚本.js
+++ 万能视频下载脚本.js
@@ -14,3 +14,13 @@ var videoElement = document.querySelector('video');
 if (videoElement) {
   videoElement.playbackRate = 2.0;
 }
+//找视频但是不下载
+var videoElement = document.querySelector('video');
+if (videoElement) {
+  var videoURL = videoElement.src;
+  var downloadLink = document.createElement('a');
+  downloadLink.href = videoURL;
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
